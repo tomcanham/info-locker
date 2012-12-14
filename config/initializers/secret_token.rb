ef861158b64c8e7378211448d2100c67df443f1f
Tom Canham <tomc@ricksteves.com>
2012-12-14 06:33:08 -0800

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InfoLocker::Application.config.secret_token = '01eb74c6a104e0c0008258df92c8a91f0b387b0d51f7a0498f88f1d8f39f91d0f823c027d46fa54e69af085d1a384a2002df615672c452c8bab9670f90ca549f'
